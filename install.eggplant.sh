57bcbfb0cf5b976739df6d60bbc2e579a08c46b9
mrellis96 <82981124+mrellis96@users.noreply.github.com>
2026-06-26 11:52:43 +1000

    Add files via upload

--- install.eggplant.sh
+++ install.eggplant.sh
@@ -18,12 +18,11 @@ tar -xf EGgPLantV*.tar.xz
 	export PATH=$PATH:"$pwd"
 	read -p "Add path to bashrc? (Y/N/C)"
 		if [[ $REPLY =~ ^[Yy]$ ]];
-		then
-		echo "export PATH=$PATH:"$pwd" >> $HOME/.bashrc"
+			then
+			echo "export PATH=$PATH:"$pwd" >> $HOME/.bashrc"
 		elif [[ $REPLY =~ ^[Cc]$ ]]; then
-		exit
+			exit
 		fi
-	fi
 	echo "Checking packages"
 	#Check if packages are installed
  	echo "r-base"
@@ -45,247 +44,249 @@ tar -xf EGgPLantV*.tar.xz
             	
             	echo "installing packages"
             	sudo apt-get install r-base r-base-core r-recommended r-cran-dev 
-        else
+    	else
         echo    "Installed"
         echo
    
-	fi
-    	echo "libcurl4-openssl-dev"
-    	which curl-config &> /dev/null
+		fi
+
+    echo "libcurl4-openssl-dev"
+	which curl-config &> /dev/null
     	if [ $? -ne 0 ]
     		then
     		echo "Not installed"
     		sudo apt-get install libcurl4-openssl-dev
     	else
-    	echo "Installed"
-    	echo
- 	fi   		
-    	echo "libxml2-dev"
-    	which xml2-config &> /dev/null
+    		echo "Installed"
+    		echo
+ 		fi   		
+    echo "libxml2-dev"
+    which xml2-config &> /dev/null
     	if [ $? -ne 0 ]
     		then
-		echo "Not installed"
+			echo "Not installed"
     		sudo apt-get install libxml2-dev
-    		else
+    	else
     		echo "Installed"
-    	echo
- 	fi   	
+    		echo
+ 		fi   	
  	echo "libssl-dev"
  	dpkg -s libssl-dev &> /dev/null
- 	if [ $? -ne 0 ]
+ 		if [ $? -ne 0 ]
  		then
- 		echo "Not installed"
- 		sudo apt-get install libssl-dev
- 	else
- 	echo "Installed"
- 	echo
- 	fi
+ 			echo "Not installed"
+ 			sudo apt-get install libssl-dev
+ 		else
+ 			echo "Installed"
+ 			echo
+ 		fi
  	
- 	echo "Blast+"
-     	which blastn &> /dev/null  
+ 			echo "Blast+"
+     		which blastn &> /dev/null  
     	if [ $? -ne 0 ]
-        	then
-            	echo "Not installed"  
-           	sudo apt-get install ncbi-blast+
-        else
-        echo    "Installed"
-        echo
-	fi
+    	   	then
+    	    echo "Not installed"  
+    	    sudo apt-get install ncbi-blast+
+    	else
+    	    echo    "Installed"
+    	    echo
+		fi
 	
 	echo "Entrez Direct"
 	which esearch &> /dev/null 
     	if [ $? -ne 0 ]
-        	then
-            	echo "Not installed"  
-            	sudo apt-get install ncbi-entrez-direct   	
-        else
-        echo    "Installed"
-        echo
-	fi
+    	   	then
+    	   	echo "Not installed"  
+    	   	sudo apt-get install ncbi-entrez-direct   	
+    	else
+    	    echo    "Installed"
+    	    echo
+		fi
 	
 	echo "vsearch"
 	which vsearch &> /dev/null 
     	if [ $? -ne 0 ]
-        	then
-            	echo "Not installed"  
-            	sudo apt-get install vsearch    	
-         	else
+    	   	then
+           	echo "Not installed"  
+           	sudo apt-get install vsearch    	
+    	else
         	echo    "Installed"
         	echo
-	fi
+		fi
 	
 	echo "Cutadapt"
 	which cutadapt &> /dev/null
-	if [ $? -ne 0 ]
-		then
-		echo "Not installed"
-		sudo apt install python3-pip
-		pip install --upgrade --user pip
-		python3 -m pip install --user --upgrade cutadapt
-		echo "Adding cutadapt to PATH"
-		export PATH=$PATH:$HOME/.local/bin
-		echo "export PATH=$PATH:$HOME/.local/bin" >> $HOME/.bashrc
+		if [ $? -ne 0 ]
+			then
+			echo "Not installed"
+			sudo apt install python3-pip
+			pip install --upgrade --user pip
+			python3 -m pip install --user --upgrade cutadapt
+			echo "Adding cutadapt to PATH"
+			export PATH=$PATH:$HOME/.local/bin
+			echo "export PATH=$PATH:$HOME/.local/bin" >> $HOME/.bashrc
 		else
-		echo "Installed"
-		echo
-	fi
+			echo "Installed"
+			echo
+		fi
+	
 	echo "taxonkit"
     which taxonkit &> /dev/null  
-    if [ $? -ne 0 ]
-        then
-        echo "Not installed"  
-        wget https://github.com/shenwei356/taxonkit/releases/download/v0.18.0/taxonkit_linux_amd64.tar.gz
-		tar -zxvf *.tar.gz
-		if [ ! -d "$HOME/bin" ]
-			then
-			echo "Personal Library not found"
-			read -p "Would you like to create a personal library ‘~$HOME/bin’  (Y/N)"
-			echo    # (optional) move to a new line 
-			if [[ $REPLY =~ ^[Yy]$ ]]
+    	if [ $? -ne 0 ]
+        	then
+        	echo "Not installed"  
+    	    wget https://github.com/shenwei356/taxonkit/releases/download/v0.18.0/taxonkit_linux_amd64.tar.gz
+			tar -zxvf *.tar.gz
+			if [ ! -d "$HOME/bin" ]
 				then
-				mkdir -p ~$HOME/bin
+				echo "Personal Library not found"
+				read -p "Would you like to create a personal library ‘~$HOME/bin’  (Y/N)"
+				echo    # (optional) move to a new line 
+				if [[ $REPLY =~ ^[Yy]$ ]]
+					then
+					mkdir -p ~$HOME/bin
+					cp taxonkit $HOME/bin
+					export PATH=$PATH:$HOME/bin
+					echo "please add 'export PATH=$PATH:$HOME/bin' to your .profile/.bachrc"
+				else
+					echo "Please add path/to/taxonkit to your PATH"
+				fi
+			else echo "Personal library Found"
 				cp taxonkit $HOME/bin
-				export PATH=$PATH:$HOME/bin
-				echo "please add 'export PATH=$PATH:$HOME/bin' to your .profile/.bachrc"
-			else
-				echo "Please add path/to/taxonkit to your PATH"
-			fi
-		else echo "Personal library Found"
-			cp taxonkit $HOME/bin
-			echo
-		fi   	
-    else
-        echo    "Installed"
-	fi
-fi
+				echo
+			fi   	
+    	else
+    	    echo    "Installed"
+		fi
 	
 	echo "R Personal lib"
 	plib="~/R/x86_64-pc-linux-gnu-library/4.0"
-	if [ ! -d "$HOME/R/x86_64-pc-linux-gnu-library/4.0" ]
-		then
-		echo "Personal Library not found"
-		read -p "Would you like to create a personal library ‘~/R/x86_64-pc-linux-gnu-library/4.0’ to install R Packages? (Y/N)"
-		echo    # (optional) move to a new line
-		if [[ $REPLY =~ ^[Yy]$ ]]
+		if [ ! -d "$HOME/R/x86_64-pc-linux-gnu-library/4.0" ]
 			then
-			mkdir -p ~/R/x86_64-pc-linux-gnu-library/4.0
+			echo "Personal Library not found"
+			read -p "Would you like to create a personal library ‘~/R/x86_64-pc-linux-gnu-library/4.0’ to install R Packages? (Y/N)"
+			echo    # (optional) move to a new line
+			if [[ $REPLY =~ ^[Yy]$ ]]
+				then
+				mkdir -p ~/R/x86_64-pc-linux-gnu-library/4.0
 			else
-			echo "Please ensure a writable library is available for R to install packages"
-		fi
+				echo "Please ensure a writable library is available for R to install packages"
+			fi
 		else echo "Personal library Found"
-		echo
-	fi
+			echo
+		fi
 	
-elif [[ "$(uname)" == "Darwin" ]];
-	then
-	echo "Installing scripts (Mac OS)"
-	cd EGgPLant/
-	chmod +x eggplant eggqual eggdb EGPEPM EGSEPM EGSEPQM EGPEPQM
-	export PATH=$PATH:"$pwd"
-	read -p "Add path to bashrc? (Y/N/C)"
-		if [[ $REPLY =~ ^[Yy]$ ]];
+	elif [[ "$(uname)" == "Darwin" ]];
 		then
-		echo "export PATH=$PATH:"$pwd" >> $HOME/.bash_profile"
+		echo "Installing scripts (Mac OS)"
+		cd EGgPLant/
+		chmod +x eggplant eggqual eggdb EGPEPM EGSEPM EGSEPQM EGPEPQM
+		export PATH=$PATH:"$pwd"
+		read -p "Add path to bashrc? (Y/N/C)"
+		if [[ $REPLY =~ ^[Yy]$ ]];
+			then
+			echo "export PATH=$PATH:"$pwd" >> $HOME/.bash_profile"
 		elif [[ $REPLY =~ ^[Cc]$ ]]; then
-		exit
+			exit
 		fi
 	
 	
-	echo "Checking packages"
-	echo "r-base"
-     	which R &> /dev/null  
+		echo "Checking packages"
+		echo "r-base"
+	     	which R &> /dev/null  
     	if [ $? -ne 0 ]
-        	then
-            	echo "Not installed"  
+       	 	then
+            echo "Not installed"  
            	which brew &> /dev/null 
-           	if [ $? -ne 0 ]
-           		then
-			echo "Installing Homebrew (Required to install packages)"
-			-c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
-		fi              
-		brew install r         	
-           	else
+       		if [ $? -ne 0 ]
+		 		then
+				echo "Installing Homebrew (Required to install packages)"
+				-c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+			fi              
+			brew install r         	
+    	else
         	echo    "Installed"
-	fi
+		fi
 	echo "Blast+"
      	which blastn &> /dev/null  
     	if [ $? -ne 0 ]
         	then
             	echo "Not installed"  
            	which brew &> /dev/null 
-           	if [ $? -ne 0 ]
+       		if [ $? -ne 0 ]
            		then
-			echo "Installing Homebrew (Required to install packages)"
-			-c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
-           	fi
+				echo "Installing Homebrew (Required to install packages)"
+				-c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+       		fi
            	brew install blast          	
-           	else
+    	else
         	echo    "Installed"
-	fi
+		fi
 	
-	echo "Entrez Direct"
+		echo "Entrez Direct"
      	which esearch &> /dev/null  
     	if [ $? -ne 0 ]
         	then
             	echo "Not installed" 
             	cwd=$(pwd)
             	cd ~
-		/bin/bash
-		perl -MNet::FTP -e \
+			/bin/bash
+			perl -MNet::FTP -e \
 		    '$ftp = new Net::FTP("ftp.ncbi.nlm.nih.gov", Passive => 1);
 		    $ftp->login; $ftp->binary;
 		    $ftp->get("/entrez/entrezdirect/edirect.tar.gz");'
-		gunzip -c edirect.tar.gz | tar xf -
-		rm edirect.tar.gz
-		builtin exit
-		export PATH=$PATH:$HOME/edirect >& /dev/null || setenv PATH "${PATH}:$HOME/edirect"
-		./edirect/setup.sh
-		echo "export PATH=\$PATH:\$HOME/edirect" >> $HOME/.profile
-		cd $cwd
-         	else
+			gunzip -c edirect.tar.gz | tar xf -
+			rm edirect.tar.gz
+			builtin exit
+			export PATH=$PATH:$HOME/edirect >& /dev/null || setenv PATH "${PATH}:$HOME/edirect"
+			./edirect/setup.sh
+			echo "export PATH=\$PATH:\$HOME/edirect" >> $HOME/.profile
+			cd $cwd
+    	else
         	echo    "Installed"
-	fi
+		fi
 	echo "Cutadapt"
 	which cutadapt &> /dev/null
-	if [ $? -ne 0 ]
-		then
-		echo "Not installed"
-		pip install --upgrade --user pip
-		python3 -m pip install --user --upgrade cutadapt
+		if [ $? -ne 0 ]
+			then
+			echo "Not installed"
+			pip install --upgrade --user pip
+			python3 -m pip install --user --upgrade cutadapt
 		else
-		echo "Installed"
-		echo
-	fi
-	echo "vsearch"
-     	which vsearch &> /dev/null  
+			echo "Installed"
+			echo
+		fi
+		echo "vsearch"
+    	 	which vsearch &> /dev/null  
     	if [ $? -ne 0 ]
         	then
             	echo "Not installed"  
-           	which brew &> /dev/null 
-           	if [ $? -ne 0 ]
+        	   	which brew &> /dev/null 
+       		if [ $? -ne 0 ]
            		then
-			echo "Installing Homebrew (Required to install packages)"
-			-c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
-           	fi
-           	brew install vsearch          	
-           	else
+				echo "Installing Homebrew (Required to install packages)"
+				-c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+    	   	fi
+    	       	brew install vsearch          	
+    	else
         	echo    "Installed"
-	fi
+		fi
 	echo "taxonkit"
      	which taxonkit &> /dev/null  
     	if [ $? -ne 0 ]
         	then
             	echo "Not installed"  
            	which brew &> /dev/null 
-           	if [ $? -ne 0 ]
+       		if [ $? -ne 0 ]
            		then
-			echo "Installing Homebrew (Required to install packages)"
-			-c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
-           	fi
-           	brew install brewsci/bio/taxonkit          	
-           	else
+				echo "Installing Homebrew (Required to install packages)"
+				-c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+    	   	fi
+       	   	brew install brewsci/bio/taxonkit          	
+    	else
         	echo    "Installed"
+		fi
 	fi
-fi
 echo "EGgPLant and all dependencies are now installed"
 echo "Please read the readme (EGgPLant_README.txt) found in "$cwd" before use"
+fi
